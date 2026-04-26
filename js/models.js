30e389bf63ea1213de8043cf8fc2cdf92848b0b5
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-26 22:06:33 +0200

    models.js | terminada funcion generarIdTareas

--- js/models.js
+++ js/models.js
@@ -21,5 +21,10 @@ export function generarIDTarea(tareas){
     if (!tareas || tareas.length === 0) {
         return "task-001";
     }
+    const ultimaTarea = tareas[tareas.length - 1];
+    const numeroString = ultimaTarea.id.slice(5);
+    const nuevoNumero = parseInt(numeroStr, 10) + 1;
+    const nuevoNumeroString = nuevoNumero.toString().padStart(3, '0');
     
+    return `task-${nuevoNumeroString}`;
 }
