6194320e811350fb798c0007bfa9b9dea0d83412
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-26 21:38:19 +0200

    funcion obtenerDatos storage.js

--- js/storage.js
+++ js/storage.js
@@ -6,3 +6,15 @@ export function guardarDatos(clave, datos) {
         console.error("Error al guardar en localStorage:", error);
     }
 }
+
+export function obtenerDatos(clave) {
+    const datosString = localStorage.getItem(clave);
+    if (!datosString) return null;
+
+    try {
+        return JSON.parse(datosString);
+    } catch (error) {
+        console.error("Error parseando datos de localStorage:", error);
+        return null;
+    }
+}
