e2729ead00668c30b843bd26f052a72876a2a09f
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-26 21:48:13 +0200

    models.js | clase tarea

--- js/models.js
+++ js/models.js
@@ -4,3 +4,15 @@ export class Categoria {
         this.color = color;
     }
 }
+
+export class Tarea {
+    constructor(id, titulo, descripcion, fecha, categoria, prioridad) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.categoria = categoria;
+        this.prioridad = prioridad;
+        this.realizada = false; // al crear una tarea, sale como no hecha
+    }
+}
