3d1670c2a88611d3ca631fb928e94621a28bd4e0
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-26 22:21:10 +0200

    categories.js | seleccionado el boton de crear categoria y el trigger.

---
+++ js/categories.js
@@ -0,0 +1,7 @@
+const btnAñadirCategoria = document.getElementById("btn-añadirCategoria");
+
+btnAñadirCategoria.addEventListener("click", function() {
+    const categoria = document.getElementById("categoria").value;
+    const color = document.getElementById("colorPicker").value;
+    console.log(`Nombre: ${nombre}, Color: ${color}`);
+});
