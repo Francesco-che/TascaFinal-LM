81f258dd05e0b5ad2733d9fd5c90efa59dcb75e5
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-27 20:18:14 +0200

    categories.js | creada instancia en el evento del boton añadir categoria

--- js/categories.js
+++ js/categories.js
@@ -1,4 +1,4 @@
-import {categoria} from './models';
+import {Categoria} from './models';
 
 const btnAñadirCategoria = document.getElementById("btn-añadirCategoria");
 
@@ -6,4 +6,7 @@ btnAñadirCategoria.addEventListener("click", function() {
     const categoria = document.getElementById("categoria").value;
     const color = document.getElementById("colorPicker").value;
     console.log(`Nombre: ${nombre}, Color: ${color}`);
+
+    const nuevaCategoria = new Categoria(nombre, color);
+    console.log("Categoría creada en memoria:", nuevaCategoria);
 });
