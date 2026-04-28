17f82d4ebecb2bee525580f7ac8dc0888e9dcbd1
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-28 21:50:52 +0200

    form-tasca.js | funcion cargarCategoriasSelect

--- js/form-tasca.js
+++ js/form-tasca.js
@@ -1,3 +1,16 @@
 import { Tarea, generarIDTarea } from './models.js';
 import {Categoria} from './models.js';
 import { obtenerDatos, guardarDatos } from './storage.js';
+
+const selectCategoria = document.getElementById("category");
+
+function cargarCategoriasSelect() {
+    const listaCategorias = obtenerDatos('categorias') || [];
+    
+    listaCategorias.forEach(categoria => {
+        const option = document.createElement("option");
+        option.value = categoria.nombre;
+        option.textContent = categoria.nombre;
+        selectCategoria.appendChild(option);
+    });
+}
