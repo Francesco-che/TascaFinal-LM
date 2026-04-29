99dea9325fc46c57cfdf0bc48d53a8ee71285c75
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-29 11:08:27 +0200

    index.js | inner.HTML avanzando, faltan las imagenes svg entre otras cosas

--- js/index.js
+++ js/index.js
@@ -20,7 +20,32 @@ function pintarTareas(){
         const tareaCreada = document.createElement("div");
         tareaCreada.classList.add("tareaCreadaPendiente");
 
-
+        tareaCreada.innerHTML = `
+            <div class="tarea-pendiente-info">
+                <div class="tarea-pendiente-info-titulo">
+                    <div>${tarea.titulo}</div>
+                    <div>${tarea.prioridad}</div>
+                </div>
+                <div class="tarea-pendiente-info-categoria" style="background-color: ${categoria.color};">
+                    ${tarea.categoria}
+                </div>
+                <div class="tarea-pendiente-info-fecha">
+                    ${tarea.fecha}
+                </div>
+                <div class="tarea-pendiente-info-botones">
+                    <div>${tarea.descripcion}</div>
+                    <div>
+                        <button type="button" class="boton-completarTarea">
+                            
+                        </button>
+                        <button type="button" class="boton-borrarTarea">
+                            
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `
+        divPendientes.appendChild(tareaCreada);
     });
 
     tareasCompletadas.forEach((tarea, index) => {
