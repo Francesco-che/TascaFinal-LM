4ed8d2767f893332badb65dccaafe9fc1b47273a
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-29 10:25:25 +0200

    index.js | creado elemento de array de tareas y las variables de pendientes y completadas, con .filter() se genera un array para cada tipo

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@ import { Tarea, generarIDTarea } from './models.js';
 window.onload = function() {
     
 };
+
+const listaTareas = obtenerDatos('tareas') || [];
+
+const tareasPendientes = listaTareas.filter(tarea => tarea.realizada === false);
+
+const tareasCompletadas = listaTareas.filter(tarea => tareasCompletadas.realizada === true);
