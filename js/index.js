3505e8faffc9b4400664fa40bd2858e9388086c4
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-29 11:37:12 +0200

    index.js | fix errores

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ import { obtenerDatos, guardarDatos } from './storage.js';
 import { Tarea, generarIDTarea } from './models.js';
 
 window.onload = function() {
-    
+    pintarTareas();
 };
 
 const listaTareas = obtenerDatos('tareas') || [];
 
 const tareasPendientes = listaTareas.filter(tarea => tarea.realizada === false);
-const tareasCompletadas = listaTareas.filter(tarea => tareasCompletadas.realizada === true);
+const tareasCompletadas = listaTareas.filter(tarea => tarea.realizada === true);
 
 const divPendientes = document.getElementById("lista-pendientes");
 const divAcabadas = document.getElementById("lista-acabadas");
@@ -28,17 +28,17 @@ function pintarTareas(){
 
         tareaCreada.innerHTML = `
             <div class="tarea-pendiente-info">
-                <div class="tarea-pendiente-info-titulo">
+                <div class="tarea-info-titulo">
                     <div>${tarea.titulo}</div>
                     <div>${tarea.prioridad}</div>
                 </div>
-                <div class="tarea-pendiente-info-categoria" style="background-color: ${colorFondo};">
+                <div class="tarea-info-categoria" style="background-color: ${colorFondo};">
                     ${tarea.categoria}
                 </div>
-                <div class="tarea-pendiente-info-fecha">
+                <div class="tarea-info-fecha">
                     ${tarea.fecha}
                 </div>
-                <div class="tarea-pendiente-info-botones">
+                <div class="tarea-info-botones">
                     <div>${tarea.descripcion}</div>
                     <div>
                         <button type="button" class="boton-completarTarea"></button>
@@ -51,5 +51,33 @@ function pintarTareas(){
     });
 
     tareasCompletadas.forEach((tarea, index) => {
+        const tareaCreada = document.createElement("div");
+        tareaCreada.classList.add("tareaCreadaAcabada");
+
+        const categoriaDeLaTarea = listaCategorias.find(cat => cat.nombre === tarea.categoria);
+        const colorFondo = categoriaDeLaTarea ? categoriaDeLaTarea.color : "#ccc";
+
+        tareaCreada.innerHTML = `
+            <div class="tarea-acabada-info">
+                <div class="tarea-info-titulo">
+                    <div>${tarea.titulo}</div>
+                    <div>${tarea.prioridad}</div>
+                </div>
+                <div class="tarea-info-categoria" style="background-color: ${colorFondo};">
+                    ${tarea.categoria}
+                </div>
+                <div class="tarea-info-fecha">
+                    ${tarea.fecha}
+                </div>
+                <div class="tarea-info-botones">
+                    <div>${tarea.descripcion}</div>
+                    <div>
+                        <button type="button" class="boton-descompletarTarea"></button>
+                        <button type="button" class="boton-borrarTarea"></button>
+                    </div>
+                </div>
+            </div>
+        `
+        divAcabadas.appendChild(tareaCreada);
     })
 }
