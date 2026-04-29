b477baea2f35be7a86131000e6752036dccd5926
Francesco Direnzo <francescoantoniodirenzo@paucasesnovescifp.cat>
2026-04-29 10:48:24 +0200

    index.js | funcion de pintar tareas comenzada, un forEach al array de cada tipo de tarea para mostrarlas

--- js/index.js
+++ js/index.js
@@ -8,5 +8,22 @@ window.onload = function() {
 const listaTareas = obtenerDatos('tareas') || [];
 
 const tareasPendientes = listaTareas.filter(tarea => tarea.realizada === false);
-
 const tareasCompletadas = listaTareas.filter(tarea => tareasCompletadas.realizada === true);
+
+const divPendientes = document.getElementById("lista-pendientes");
+const divAcabadas = document.getElementById("lista-acabadas");
+
+function pintarTareas(){
+    divPendientes.innerHTML = '<div id="index-tarea-pendiente"></div>';
+
+    tareasPendientes.forEach((tarea, index) => {
+        const tareaCreada = document.createElement("div");
+        tareaCreada.classList.add("tareaCreadaPendiente");
+
+
+    });
+
+    tareasCompletadas.forEach((tarea, index) => {
+        divAcabadas.innerHTML = '<div id="index-tarea-acabada"></div>';
+    })
+}
